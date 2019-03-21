96f71d1215f6a01b4c11b9b15cb5280514e0bca6
Alec Budd <abudd1094@gmail.com>
2019-03-21 11:56:40 +0100

    notes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var Emitter = require('./emitter');  // if we switch './emitter'  to  'events'  we will be using the emitter built into the code of node.js
 var eventConfig = require('./config').events // grab events property from the config object
 
-var emtr = new Emitter();
+var emtr = new Emitter();  // create a new instance of an event emitter
 
 emtr.on(eventConfig.GREET, function() {  // we can replace the 'greet' string with a value from the config object's event property, this helps us avoid issues with magic strings
   console.log('Somewhere, someone said hello.')  // add this console.log function to the 'greet' 'type' on the events object
